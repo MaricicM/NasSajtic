79071f7947eb10124498964dbacc1941c9e0f635
MirkoLisanin <mirkolisanin@gmail.com>
2018-04-20 11:50:36 +0200

    first commit

---
+++ Nas Sajtic/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nas_Sajtic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Galerija()
+        {
+            return View();
+        }
+
+        public ActionResult Ponuda ()
+        {
+            return View();
+        }
+        public PartialViewResult _Ponuda1 ()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _Ponuda2()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _Ponuda3()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ONama ()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt ()
+        {
+            return View();
+        }
+
+        public ActionResult RadoviUToku ()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nas Sajtic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nas_Sajtic.MvcApplication" Language="C#" %>
